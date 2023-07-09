8df3c2a9117bc6828ec6851aef8a1546d9f2ce29
Lucas Anderson <lucas.a@comcast.net>
2023-07-08 22:04:03 -0500

    Add Screen size change

--- Script.js
+++ Script.js
@@ -8,6 +8,7 @@ const picker = document.getElementById('picker');
 const bgPicker = document.getElementById('bgpicker');
 const bgColorBtn = document.getElementById('bg-color-btn');
 const body = document.querySelector('body');
+const adjustSizeBtn = document.getElementById('adjust-size-btn');
 
 let screenSize = 16;
 let curColor = '#000000';
@@ -16,31 +17,19 @@ let isGridToggled = true;
 let isMouseDown = false;
 let isRandomColors = false;
 let isErasing = false;
+let pixelSize = '2.5vw';
 
 colorSign.style.backgroundColor = curColor;
 
 // Create pixel matrix and add listeners
-for(let i = 0; i < screenSize; i++) {
-    const div = document.createElement('div');
-    div.classList.add('row');
-    for(let j = 0; j < screenSize; j++) {
-        const pixel = document.createElement('div');
-        pixel.classList.add('pixel');
-        pixel.addEventListener('mousedown', () => {
-            if(isRandomColors) {
-                pixel.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-            } else if(isErasing) {
-                pixel.style.backgroundColor = curBGColor;
-            } else {
-                pixel.style.backgroundColor = curColor;
-            }
-            isMouseDown = true;
-        });
-        body.addEventListener('mouseup', () => {
-            isMouseDown = false;
-        });
-        pixel.addEventListener('mouseenter', () => {
-            if(isMouseDown) {
+function makeScreen() {
+    for(let i = 0; i < screenSize; i++) {
+        const div = document.createElement('div');
+        div.classList.add('row');
+        for(let j = 0; j < screenSize; j++) {
+            const pixel = document.createElement('div');
+            pixel.classList.add('pixel');
+            pixel.addEventListener('mousedown', () => {
                 if(isRandomColors) {
                     pixel.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
                 } else if(isErasing) {
@@ -48,11 +37,26 @@ for(let i = 0; i < screenSize; i++) {
                 } else {
                     pixel.style.backgroundColor = curColor;
                 }
-            }
-        });
-        div.appendChild(pixel);
+                isMouseDown = true;
+            });
+            body.addEventListener('mouseup', () => {
+                isMouseDown = false;
+            });
+            pixel.addEventListener('mouseenter', () => {
+                if(isMouseDown) {
+                    if(isRandomColors) {
+                        pixel.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+                    } else if(isErasing) {
+                        pixel.style.backgroundColor = curBGColor;
+                    } else {
+                        pixel.style.backgroundColor = curColor;
+                    }
+                }
+            });
+            div.appendChild(pixel);
+        }
+        screen.appendChild(div);
     }
-    screen.appendChild(div);
 }
 
 clearbtn.addEventListener('click', clear);
@@ -96,13 +100,22 @@ bgPicker.addEventListener('change', (e) => {
     curBGColor = e.target.value;
     clear();
 })
-function updateSize() {
+adjustSizeBtn.addEventListener('click', () => {
+   screenSize = prompt("Warning! inputs above 64x64 may slow down computer\nInput size: ");
+   while(screen.firstChild) {
+       screen.removeChild(screen.firstChild);
+   }
+   makeScreen();
+   clear();
+});
 
-}
 function clear() {
     let grid = document.querySelectorAll(".pixel");
     for(let i = 0; i < grid.length; i++) {
+        grid[i].style.width = 40 / screenSize + "vw";
+        grid[i].style.height = 40 / screenSize + "vw";
         grid[i].style.backgroundColor = curBGColor;
     }
 }
+makeScreen();
 clear();
